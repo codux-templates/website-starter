c4e77d893ce3b5b07d9cad4c8eaf5fd3486edd63
ShireeWill <shiree@wix.com>
2024-01-01 15:44:37 +0200

    added paragraph

--- src/components/test/test.tsx
+++ src/components/test/test.tsx
@@ -10,5 +10,10 @@ export interface TestProps {
  * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
  */
 export const Test = ({ className }: TestProps) => {
-    return <div className={classNames(styles.root, className)}>Test</div>;
+    return <div className={classNames(styles.root, className)}>
+        <div className={styles.title}>I&apos;M A TITLE</div>
+        <div className={styles.paragraph}>
+            <div className={styles.text}>
+                I’m a paragraph. Add your own text and edit me on properties panel on the right. It’s easy. Just select me and make changes to the font on the styles panel on the right. I’m a great place for you to tell a story and let your viewers know a little more about you.</div></div>
+    </div>;
 };
