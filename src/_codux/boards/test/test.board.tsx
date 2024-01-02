a91580273218385ea4631481d31534d0c1ac9190
alissawix <alissav@wix.com>
2024-01-02 13:05:10 +0200

    delete test board

--- src/_codux/boards/test/test.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { HomePage } from '../../../components/home-page/home-page';
-
-export default createBoard({
-    name: 'Test',
-    Board: () => <HomePage />,
-    isSnippet: true,
-});
