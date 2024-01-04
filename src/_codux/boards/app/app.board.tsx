0827f94d72a83bbf1af100ef52504ec910f7e943
alissawix <alissav@wix.com>
2024-01-04 16:46:53 +0200

    add margin 0 to board canvas

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -4,4 +4,12 @@ import { PagewithNavigation } from '../../board-wrappers/page-with-navigation';
 export default createBoard({
     name: 'App',
     Board: () => <PagewithNavigation />,
+    environmentProps: {
+        canvasMargin: {
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+        }
+    }
 });
